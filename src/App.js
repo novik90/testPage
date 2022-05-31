464f9ec36660077ced968f387d291900ba52ab91
novik90 <ivan.novikov.1990@list.ru>
2022-05-31 18:31:31 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewTodo, fetchTodos } from "./store/todoSlice";
+
+import TodoList from "./components/TodoList";
+import InputField from "./components/InputField";
 import "./App.css";
 
 function App() {
+    const { status, error } = useSelector((state) => state.todos);
+    const dispatch = useDispatch();
+    const [text, setText] = useState("");
+
+    const addTask = () => {
+        dispatch(addNewTodo(text));
+        setText("");
+    };
+
+    useEffect(() => {
+        dispatch(fetchTodos());
+    }, [dispatch]);
+
     return (
-        <div>
-            <p>redux slices 2</p>
-            <p>redux slices 2</p>
-            <p>redux slices 2</p>
+        <div className="App">
+            <InputField
+                text={text}
+                handleInput={setText}
+                handleSubmit={addTask}
+            />
+
+            {status === "loading" && <h2>Loading...</h2>}
+            {error && <h2>An error occured: {error}</h2>}
+
+            <TodoList />
         </div>
     );
 }
